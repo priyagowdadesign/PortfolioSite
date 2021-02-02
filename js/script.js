1527ac4a4c2b77bf9662e6f25e3ac03d1cb03cb8
Priya Gowda <77813128+priyagowda@users.noreply.github.com>
2021-02-02 17:43:38 -0500

    Fix menu issue for Desktop version
    Fix menu issue for Desktop version

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 console.log("Hi there, Welcome to my Portfolio site! - Priya Bachegowda")
 
 function menuToggle() {
-  var x = document.getElementById('myNavtoggle')
+
+  var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
     x.className += ' responsive'
   } else {
     x.className = 'navtoggle'
+  }
+
+}
+
+function closeMenu(){
+  if($(".fa-bars").is(":visible")){
+    $("#myNavtoggle").attr("class","navtoggle");
   }
 }
