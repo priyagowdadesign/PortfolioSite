3cb8191de7e96f42ca441017468ef0b214214aa6
Priya Gowda <77813128+priyagowda@users.noreply.github.com>
2021-02-07 01:49:57 -0500

    Code linting
    Utilize code linting to avoid bugs and ensure consistent code quality

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-console.log("Hi there, Welcome to my Portfolio site! - Priya Bachegowda")
+import $ from 'jquery'
+/* exported menuToggle, closeMenu */
+console.log('Hi there, Welcome to my Portfolio site! - Priya Bachegowda')
 
-function menuToggle() {
-
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
     x.className += ' responsive'
   } else {
     x.className = 'navtoggle'
   }
-
 }
 
-function closeMenu(){
-  if($(".fa-bars").is(":visible")){
-    $("#myNavtoggle").attr("class","navtoggle");
+function closeMenu () {
+  if ($('.fa-bars').is(':visible')) {
+    $('#myNavtoggle').attr('class', 'navtoggle')
   }
 }
